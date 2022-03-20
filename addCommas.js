fc759009eb99853b6a596738286da9d5b6de0a0d
Jingyi Lin <linjingyikayla@gmail.com>
2022-03-19 22:19:55 -0400

    create addCommas Function

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,34 @@
-function addCommas() {}
+function addCommas(nums) {
+    let res = nums < 0 ? "-" : "";
+
+    function handleInteger(integerString) {
+        let str = "";
+        let digit = 0;
+        for (let i = integerString.length - 1; i >= 0; i--){
+            str += integerString[i];
+            digit += 1;
+            if (i === 0){
+                continue;
+            }
+            if (digit % 3 === 0){
+                str += ",";
+            }
+        }
+        return str.split("").reverse().join("");
+    }
+
+    if (!Number.isInteger(nums)) {
+        let arr = Math.abs(nums).toString().split(".");
+        integerString = arr[0];
+        bonusString = arr[1];
+        return res + handleInteger(integerString) + "." + bonusString;
+    }
+    else {
+        return res + handleInteger(Math.abs(nums).toString());
+    }
+
+
+}
+
 
 module.exports = addCommas;
--- addCommas.test.js
+++ addCommas.test.js
@@ -4,4 +4,19 @@ describe("#addCommas", () => {
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
   });
+  test("function works", () => { 
+    expect(addCommas(3000)).toBe("3,000");
+  });
+  test("function works", () => { 
+    expect(addCommas(-21)).toBe("-21");
+  });
+  test("function works", () => { 
+    expect(addCommas(9876543210)).toBe("9,876,543,210");
+  });
+  test("function works", () => { 
+    expect(addCommas(300)).toBe("300");
+  });
+  test("function works", () => { 
+    expect(addCommas(-3141592.65)).toBe("-3,141,592.65");
+  });
 });
